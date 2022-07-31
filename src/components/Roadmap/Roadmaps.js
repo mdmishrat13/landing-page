19136bc091e1be8d0cc7036a4653e760d51752d4
mdmishrat13 <mdmishrat13@gmail.com>
2022-07-31 19:17:53 +0600

    added some styse

--- src/components/Roadmap/Roadmaps.js
+++ src/components/Roadmap/Roadmaps.js
@@ -3,6 +3,8 @@ import Roadmap from "./Roadmap";
 import "./roadmap.css";
 import section from "./../../images/section.png";
 import sectionStyle from "./../../images/teamTitle.png";
+import vector4 from "./../../images/vector4.png";
+import roadmapLine1 from './../../images/roadmapLine1.png'
 
 const Roadmaps = () => {
   const steps = [
@@ -21,77 +23,85 @@ const Roadmaps = () => {
   ];
   return (
     <div className="roadmap">
-      
       <div className="roadmap-cards">
         {/* {steps.map(step=><Roadmap key={step.id} step= {step}></Roadmap>)} */}
-        <div className="roadmap-card">
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with distinguished personalities across many avenues
-            including trusted individuals within the NFT community, celebrities,
-            artists, influencers and other NFT projects.
-          </p>
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with{" "}
-          </p>
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with This period we look to plan and collaborate using
-            strategic partnerships with{" "}
-          </p>
-        </div>
+        <img className='section-line'src={roadmapLine1} alt="" />
+        <div className="roadmap-card-container">
+          <img className="background-vector" src={vector4} alt="" />
 
-        <div className="team-title">
-        <img src={sectionStyle} alt="" />
-        <div className="texts">
-          <h1> THE AIKUJI</h1>
-          <h2>TEAM</h2>
+          <div className="roadmap-card">
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with distinguished personalities across many avenues
+              including trusted individuals within the NFT community,
+              celebrities, artists, influencers and other NFT projects.
+            </p>
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with{" "}
+            </p>
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with This period we look to plan and collaborate
+              using strategic partnerships with{" "}
+            </p>
+          </div>
+            
         </div>
-      </div>
+        <div className="middle-roadmap">
+        <div className="roadmap-card-container">
+        <img className="background-vector" src={vector4} alt="" />
+          <div className="roadmap-card">
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with distinguished personalities across many avenues
+              including trusted individuals within the NFT community,
+              celebrities, artists, influencers and other NFT projects.
+            </p>
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with{" "}
+            </p>
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with This period we look to plan and collaborate
+              using strategic partnerships with{" "}
+            </p>
+          </div>
+        </div>
+            </div>
+        
 
-        <div className="roadmap-card">
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with distinguished personalities across many avenues
-            including trusted individuals within the NFT community, celebrities,
-            artists, influencers and other NFT projects.
-          </p>
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with{" "}
-          </p>
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with This period we look to plan and collaborate using
-            strategic partnerships with{" "}
-          </p>
+        <div className="last-roadmap">
+        <div className="roadmap-card-container">
+        <img className="background-vector" src={vector4} alt="" />
+          <div className="roadmap-card">
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with distinguished personalities across many avenues
+              including trusted individuals within the NFT community,
+              celebrities, artists, influencers and other NFT projects.
+            </p>
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with{" "}
+            </p>
+            <h1>AIKUJI TIME :</h1>
+            <p>
+              This period we look to plan and collaborate using strategic
+              partnerships with This period we look to plan and collaborate
+              using strategic partnerships with{" "}
+            </p>
+          </div>
         </div>
-        <div className="roadmap-card">
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with distinguished personalities across many avenues
-            including trusted individuals within the NFT community, celebrities,
-            artists, influencers and other NFT projects.
-          </p>
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with{" "}
-          </p>
-          <h1>AIKUJI TIME :</h1>
-          <p>
-            This period we look to plan and collaborate using strategic
-            partnerships with This period we look to plan and collaborate using
-            strategic partnerships with{" "}
-          </p>
         </div>
       </div>
       <div className="roadmap-section">
--- src/components/Team/TeamCard.js
+++ src/components/Team/TeamCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./team.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord } from '@fortawesome/fontawesome-free-brands';
 import avatar1 from "./../../images/avatar1.png";
+import vector3 from "./../../images/Vector3.png";
 
 const TeamCard = ({ person }) => {
   return (
     <div>
       <div className="team-card">
-        </div>
-      <div className="team-card2">
-        </div>
-        
+        <img src={vector3} alt="" />
         <div className="main-identity">
           <img className="avatar" src={avatar1} alt="" />
           <h2>RUDY</h2>
@@ -23,9 +23,11 @@ const TeamCard = ({ person }) => {
             associated with AIKUJI.
           </p>
           <h2>Founder</h2>
+          <div className="contact-icons">
+          <FontAwesomeIcon icon={faDiscord} />
+          </div>
         </div>
-        {/* </div> */}
-      {/* </div> */}
+      </div>
     </div>
   );
 };
